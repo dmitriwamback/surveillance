3a161af687bf71c59ca72bb3a8cdba03dc5d1a17
Dmitri Wamback <56200546+DmitriWamback@users.noreply.github.com>
2024-03-11 20:59:03 -0400

    .

--- src/core/Assigner.java
+++ src/core/Assigner.java
@@ -3,6 +3,20 @@ package core;
 import core.profiler.Profile;
 
 public class Assigner {
+
+    public enum Trust {
+        Dishonor,
+        Inferiority,
+        Incompetent,
+        Imperfection,
+        Dissatisfaction,
+    Neutral,
+        Good,
+        Satisfactory,
+        Merit,
+        Excellence,
+        Honored,
+    }
     
     public void assignUserStatus(int userUniqueId, String inputPrivateKey) {
 
--- src/core/profiler/Profile.java
+++ src/core/profiler/Profile.java
@@ -1,35 +1,21 @@
 package core.profiler;
 
 import java.util.Random;
+import core.Assigner;
 
 public class Profile {
 
 
-    enum Trust {
-            Dishonor,
-            Inferiority,
-            Incompetent,
-            Imperfection,
-            Dissatisfaction,
-        Neutral,
-            Good,
-            Satisfactory,
-            Merit,
-            Excellence,
-            Honored,
-    }
+    public String           firstName, 
+                            lastName;
 
+    public int              age;
+    public int              score;
+    public int              uniqueId;
+    public String           uniqueIdentifier;
+    public String           nationality;
 
-    public String   firstName, 
-                    lastName;
-
-    public int      age;
-    public int      score;
-    public int      uniqueId;
-    public String   uniqueIdentifier;
-    public String   nationality;
-
-    public Trust    trustworthiness;
+    public Assigner.Trust   trustworthiness;
 
     public void initializeProfile(String firstName, String lastName, String nationality, int age) {
 
@@ -40,7 +26,7 @@ public class Profile {
         this.age                = age;
         this.nationality        = nationality;
         this.score              = 750;
-        this.trustworthiness    = Trust.Neutral;
+        this.trustworthiness    = Assigner.Trust.Neutral;
 
         double n = new Random().nextDouble();
         while (n <= 0.0) {
@@ -58,19 +44,19 @@ public class Profile {
 
     private void modifyTrustworthiness() {
 
-        if (score <= 800  && score > 700)    trustworthiness = Trust.Neutral;
-        if (score <= 900  && score > 800)    trustworthiness = Trust.Good;
-        if (score <= 1100 && score > 900)    trustworthiness = Trust.Satisfactory;
-        if (score <= 1200 && score > 1100)   trustworthiness = Trust.Merit;
-        if (score <= 1300 && score > 1200)   trustworthiness = Trust.Excellence;
-        if (score <= 1500 && score > 1400)   trustworthiness = Trust.Honored;
+        if (score <= 800  && score > 700)    trustworthiness = Assigner.Trust.Neutral;
+        if (score <= 900  && score > 800)    trustworthiness = Assigner.Trust.Good;
+        if (score <= 1100 && score > 900)    trustworthiness = Assigner.Trust.Satisfactory;
+        if (score <= 1200 && score > 1100)   trustworthiness = Assigner.Trust.Merit;
+        if (score <= 1300 && score > 1200)   trustworthiness = Assigner.Trust.Excellence;
+        if (score <= 1500 && score > 1400)   trustworthiness = Assigner.Trust.Honored;
 
-        if (score <= 700  && score >  600)   trustworthiness = Trust.Neutral;
-        if (score <= 600  && score >  500)   trustworthiness = Trust.Dissatisfaction;
-        if (score <= 500  && score >  300)   trustworthiness = Trust.Imperfection;
-        if (score <= 300  && score >  100)   trustworthiness = Trust.Incompetent;
-        if (score <= 100  && score > -500)   trustworthiness = Trust.Inferiority;
-        if (score <=-500)                    trustworthiness = Trust.Dishonor;
+        if (score <= 700  && score >  600)   trustworthiness = Assigner.Trust.Neutral;
+        if (score <= 600  && score >  500)   trustworthiness = Assigner.Trust.Dissatisfaction;
+        if (score <= 500  && score >  300)   trustworthiness = Assigner.Trust.Imperfection;
+        if (score <= 300  && score >  100)   trustworthiness = Assigner.Trust.Incompetent;
+        if (score <= 100  && score > -500)   trustworthiness = Assigner.Trust.Inferiority;
+        if (score <=-500)                    trustworthiness = Assigner.Trust.Dishonor;
     }
 
     public void deductScore(int score) {
